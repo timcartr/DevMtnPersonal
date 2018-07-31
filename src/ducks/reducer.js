79dd5e30685f98f3e6dc6d8385460f6f3732189f
Tim <tim.cartr@gmail.com>
2018-07-31 15:53:36 -0600

    redux set up

---
+++ src/ducks/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    daily: false,
+    monthly: false,
+    yearly: false
+}
+
+export default function reducer(state = initialState, action) {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
