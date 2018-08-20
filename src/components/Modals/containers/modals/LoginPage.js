ffe291dc109feb4b4fbebe30c04afa83ca74fea7
Tim <tim.cartr@gmail.com>
2018-08-20 11:19:45 -0600

    fixed auth0

--- src/components/Modals/containers/modals/LoginPage.js
+++ src/components/Modals/containers/modals/LoginPage.js
@@ -8,7 +8,7 @@ export default class LoginPage extends Component {
         let url = `${window.location.origin}/auth/callback`
     
         window.location = 
-        `https://${REACT_APP_DOMAIN}/authorize?client_id=${REACT_APP_CLIENT_ID}&scope=openid%20profile%20email&redirect_url=${url}&response_type=code`
+        `https://${REACT_APP_DOMAIN}/authorize?client_id=${REACT_APP_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${url}&response_type=code`
       }
 
   render() {
