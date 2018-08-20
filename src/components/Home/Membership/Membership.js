2e1e19466ebac864e0f92d9e39dea962d41a00f8
Tim <tim.cartr@gmail.com>
2018-08-20 14:54:14 -0600

    final push

--- src/components/Home/Membership/Membership.js
+++ src/components/Home/Membership/Membership.js
@@ -6,8 +6,10 @@ export default class Membership extends Component {
     render() {
         return (
             <div className='Membership'>
-            <h2>Membership</h2>
-            <p>Lorem ipsum dolor amet stumptown occupy kinfolk flexitarian, umami pug pok pok cornhole vexillologist kale chips. Poutine ethical viral cred, brunch af hot chicken. Bicycle rights health goth forage, food truck VHS plaid cray small batch tofu.</p>
+            <div className='MembershipHeader'>
+                <h2>Membership</h2>
+                <p>Lorem ipsum dolor amet stumptown occupy kinfolk flexitarian, umami pug pok pok cornhole vexillologist kale chips. Poutine ethical viral cred, brunch af hot chicken. Bicycle rights health goth forage, food truck VHS plaid cray small batch tofu.</p>
+            </div>
             <div className='MembershipFlexwrap'>
                 <Plans/>
                 <Plans/>
