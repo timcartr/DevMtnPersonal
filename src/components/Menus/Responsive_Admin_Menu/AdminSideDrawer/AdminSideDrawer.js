87c64f5cea041111a36b9c1f64f71a3eb3fe0f98
Tim <tim.cartr@gmail.com>
2018-08-20 14:24:48 -0600

    fixed sidedrawers

--- src/components/Menus/Responsive_Admin_Menu/AdminSideDrawer/AdminSideDrawer.js
+++ src/components/Menus/Responsive_Admin_Menu/AdminSideDrawer/AdminSideDrawer.js
@@ -1,31 +1,28 @@
 import React, {Component} from 'react'
-import './AdminSideDrawer.css'
+import '../../Responsive_Header/SideDrawer/SideDrawer.css'
 import {Link} from 'react-router-dom'
 
-class AdminSideDrawer extends Component {
-    
-    render(){
-        let drawerClasses = 'side-drawer'
-        if(this.props.sideDrawerOpen){
-            drawerClasses = 'side-drawer open'
-        }
-        return(
-            <nav className={drawerClasses}>
-                <div className="side-drawer-flex">
-                    <ul>                    
-                        <Link to='/admin'><li>Dashboard</li></Link>
-                        <Link to='/admin/members'><li>Members</li></Link>
-                        <Link to='/admin/metrics'><li>Metrics</li></Link>
-                        <Link to='/admin/inbox'><li>Inbox</li></Link>
-                        <Link to='/admin/activity'><li>Activity</li></Link>
-                        <Link to='/admin/classes'><li>Classes</li></Link>
-                        <Link to='/admin/marketplace'><li>Marketplace</li></Link>
-                    </ul>
-                    <div className="side-drawer-profile">Profile</div>
-                </div>
-            </nav>
-        )
+const AdminSideDrawer = props => {
+    let drawerClasses = 'side-drawer'
+    if(props.show){
+        drawerClasses = 'side-drawer open'
     }
+    return(
+        <nav className={drawerClasses}>
+        <div className="side-drawer-flex">
+            <ul>                    
+                <Link to='/admin'><li>Dashboard</li></Link>
+                <Link to='/admin/members'><li>Members</li></Link>
+                <Link to='/admin/metrics'><li>Metrics</li></Link>
+                <Link to='/admin/inbox'><li>Inbox</li></Link>
+                <Link to='/admin/activity'><li>Activity</li></Link>
+                <Link to='/admin/classes'><li>Classes</li></Link>
+                <Link to='/admin/marketplace'><li>Marketplace</li></Link>
+            </ul>
+            <div className="side-drawer-profile">Profile</div>
+        </div>
+        </nav>
+    )
 }
 
 export default AdminSideDrawer
